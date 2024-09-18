b636f6b658207e85b4272218e2297a0c6e4786eb
Quorum-Code <quorumcode@gmail.com>
2024-09-17 18:02:56 -0700

    Changed indexHandler to parse html templates

--- internal/webserver/indexHandler.go
+++ internal/webserver/indexHandler.go
@@ -2,6 +2,7 @@ package webserver
 
 import (
 	"html/template"
+	"log"
 	"net/http"
 )
 
@@ -11,6 +12,25 @@ func (wsc *WSConfig) indexHandler(resp http.ResponseWriter, req *http.Request) {
 		tmpl := template.Must(template.ParseFiles("html/page-not-found.html"))
 		tmpl.Execute(resp, nil)
 		return
+	}
+
+	files := []string{
+		"./html/index.html",
+		"./html/head.html",
+		"./html/header.html",
+	}
+
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(resp, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+
+	err = ts.ExecuteTemplate(resp, "index", nil)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(resp, "Internal server error", http.StatusInternalServerError)
 	}
 
 	tmpl := template.Must(template.ParseFiles("html/index.html"))
